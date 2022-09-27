63f8d56daaf4253251f58ed02bf7564864e3cec1
canmert <canmert.dev@gmail.com>
2022-09-27 23:41:15 +0300

    card component

---
+++ src/components/Card/card.props.ts
@@ -0,0 +1,8 @@
+export interface CardProps {
+  imageUrl: String;
+  title: String;
+  category: String;
+  date: String;
+  content: String;
+  author: String;
+}
---
+++ src/components/Card/card.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CardProps } from "./card.props";
+
+export const Card = (props: CardProps): JSX.Element => {
+  return (
+    <div className="container">
+      <div className="image">imageUrl</div>
+    </div>
+  );
+};
